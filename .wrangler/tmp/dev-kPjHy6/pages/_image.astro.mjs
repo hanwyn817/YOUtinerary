ecac8fe26deeed27c55f46f561975afa6af2611b
hanwyn817 <manhanwen@outlook.com>
2025-09-20 22:45:01 +0800

    Remove unused renderers module from temporary pages directory

--- .wrangler/tmp/dev-kPjHy6/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { a as page } from '../chunks/image-endpoint_Cg4Dbir6.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_BlVgbzts.mjs';
--- .wrangler/tmp/pages-1VQYPv/_@astrojs-ssr-adapter.mjs
+++
@@ -1,2 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { c as createExports } from './chunks/_@astrojs-ssr-adapter_Ci_7_e2f.mjs';
